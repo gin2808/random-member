761b42da6c7c519e82c9d5deffb6c843be3e9cd6
Phan Thanh Giang <gin2808@users.noreply.github.com>
2019-02-27 23:04:24 +0700

    Update random.py

--- random.py
+++ random.py
@@ -1,6 +1,6 @@
 import random as rd
 ds = ['Di', 'Duy','Giang', 'Huy','Khang', 'Khoi']
-for i in range(0, 6):    
+for i in range(0, len(ds)):    
     ten = rd.choice(ds)
     print ten
     ds.remove(ten)
